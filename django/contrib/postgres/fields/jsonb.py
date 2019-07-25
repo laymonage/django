78e5604336c9ff461f231a47b3f53eb9aaadea57
sage <laymonage@gmail.com>
2019-07-25 20:58:33 +0700

    Override deconstruct in contrib.postgres JSONField

--- django/contrib/postgres/fields/jsonb.py
+++ django/contrib/postgres/fields/jsonb.py
@@ -1,3 +1,10 @@
-from django.db.models import JSONField
+from django.db.models import JSONField as BuiltinJSONField
 
 __all__ = ['JSONField']
+
+
+class JSONField(BuiltinJSONField):
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        path = 'django.contrib.postgres.fields.JSONField'
+        return name, path, args, kwargs
