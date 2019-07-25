85b7c8a522b7281753a6ae4f1abfd694c8c09b2e
sage <laymonage@gmail.com>
2019-07-26 06:21:38 +0700

    Add TypeError to exception
    Python 3.6 uses TypeError instead of JSONDecodeError

--- django/db/models/fields/json.py
+++ django/db/models/fields/json.py
@@ -67,7 +67,7 @@ class JSONField(CheckFieldDefaultMixin, Field):
         else:
             try:
                 return json.loads(value, cls=self.decoder)
-            except json.decoder.JSONDecodeError:
+            except (json.decoder.JSONDecodeError, TypeError):
                 return value
 
     def get_internal_type(self):
