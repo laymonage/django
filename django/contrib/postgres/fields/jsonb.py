dd616fa1ffdb4c9c29085662b1583bd96751c5e2
sage <laymonage@gmail.com>
2019-07-27 17:18:21 +0700

    Add deprecation warning

--- django/contrib/postgres/fields/jsonb.py
+++ django/contrib/postgres/fields/jsonb.py
@@ -1,9 +1,20 @@
+import warnings
+
 from django.db.models import JSONField as BuiltinJSONField
+from django.utils.deprecation import RemovedInDjango40Warning
 
 __all__ = ['JSONField']
 
 
 class JSONField(BuiltinJSONField):
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            'django.contrib.postgres.fields.JSONField is deprecated in favour of '
+            'django.db.models.JSONField',
+            RemovedInDjango40Warning, stacklevel=2
+        )
+        super().__init__(*args, **kwargs)
+
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
         path = 'django.contrib.postgres.fields.JSONField'
