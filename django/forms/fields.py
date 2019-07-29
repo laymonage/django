1c2e44df0a5740ee5146a98ed9ed99b66800dc38
sage <laymonage@gmail.com>
2019-07-29 15:34:44 +0700

    Remove temporary variable in form field

--- django/forms/fields.py
+++ django/forms/fields.py
@@ -1259,8 +1259,7 @@ class JSONField(CharField):
             return True
         # For purposes of seeing whether something has changed, True isn't the
         # same as 1 and the order of keys doesn't matter.
-        data = self.to_python(data)
         return (
             json.dumps(initial, sort_keys=True, cls=self.encoder) !=
-            json.dumps(data, sort_keys=True, cls=self.encoder)
+            json.dumps(self.to_python(data), sort_keys=True, cls=self.encoder)
         )
