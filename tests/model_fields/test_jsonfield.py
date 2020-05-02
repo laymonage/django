1f3b913742a2606dcf822605889344b176a4389f
sage <laymonage@gmail.com>
2020-05-02 13:40:59 +0700

    Added deep tests for HasKeys and HasAnyKeys.

--- tests/model_fields/test_jsonfield.py
+++ tests/model_fields/test_jsonfield.py
@@ -393,11 +393,42 @@ class TestQuerying(TestCase):
             [self.objs[4]],
         )
 
+    def test_has_keys_deep(self):
+        tests = [
+            Q(value__baz__has_keys=['a', 'c']),
+            Q(value__has_keys=[
+                KeyTransform('a', KeyTransform('baz', 'value')),
+                KeyTransform('c', KeyTransform('baz', 'value'))
+            ]),
+        ]
+        for condition in tests:
+            with self.subTest(condition=condition):
+                self.assertSequenceEqual(
+                    NullableJSONModel.objects.filter(condition),
+                    [self.objs[7]],
+                )
+
     def test_has_any_keys(self):
         self.assertSequenceEqual(
             NullableJSONModel.objects.filter(value__has_any_keys=['c', 'l']),
             [self.objs[3], self.objs[4], self.objs[6]],
         )
+
+    def test_has_any_keys_deep(self):
+        obj = NullableJSONModel.objects.create(value={'baz': {'a': 'x', 'd': 'z'}})
+        tests = [
+            Q(value__baz__has_any_keys=['c', 'd']),
+            Q(value__has_any_keys=[
+                KeyTransform('c', KeyTransform('baz', 'value')),
+                KeyTransform('d', KeyTransform('baz', 'value'))
+            ]),
+        ]
+        for condition in tests:
+            with self.subTest(condition=condition):
+                self.assertSequenceEqual(
+                    NullableJSONModel.objects.filter(condition),
+                    [self.objs[7], obj],
+                )
 
     def test_contains(self):
         tests = [
