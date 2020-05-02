a14f8cd68c9d726cffd99cc24b63029aaef6569f
sage <laymonage@gmail.com>
2020-05-02 12:31:01 +0700

    Fixed HasKey lookup on JSON arrays with chained KeyTransform rhs.

--- django/db/models/fields/json.py
+++ django/db/models/fields/json.py
@@ -222,6 +222,14 @@ class HasKeyLookup(PostgresOperatorLookup):
         # as bind variables on Oracle.
         return sql % tuple(params), []
 
+    def as_postgresql(self, compiler, connection):
+        if isinstance(self.rhs, KeyTransform):
+            *_, rhs_key_transforms = self.rhs.preprocess_lhs(compiler, connection)
+            for key in rhs_key_transforms[:-1]:
+                self.lhs = KeyTransform(key, self.lhs)
+            self.rhs = rhs_key_transforms[-1]
+        return super().as_postgresql(compiler, connection)
+
     def as_sqlite(self, compiler, connection):
         return self.as_sql(compiler, connection, template='JSON_TYPE(%s, %%s) IS NOT NULL')
 
